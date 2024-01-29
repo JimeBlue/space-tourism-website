60ce4784fa0fe67f65264d3e77f8309dad0d35dc
Jimena <j.miguez@airlst.com>
2024-01-29 13:45:30 +0100

    Handle planet image on planet change up to screen size lg

--- pages/destination.vue
+++ pages/destination.vue
@@ -2,9 +2,22 @@
   <div class="bg-image bg-cover bg-no-repeat min-h-screen w-screen">
     <div class="w-11/12 lg:w-full mx-auto max-w-[1250px] py-20">
       <section class="text-white">
-        <p>01 Pick your destination</p>
+        <p class="flex justify-center space-x-2 uppercase tracking-[4px]">
+          <span class="opacity-70">01</span> <span>Pick your destination</span>
+        </p>
         <!-- Tabs component from Nuxt UI -->
         <div class="mt-24">
+          <div
+            v-for="(item, index) in items"
+            :key="item.label"
+            class="lg:hidden"
+          >
+            <img
+              :src="item.image"
+              :alt="item.label"
+              v-show="index === selectedTabIndex"
+            />
+          </div>
           <UTabs
             :items="items"
             :ui="{
@@ -18,10 +31,15 @@
                 width: 'w-full',
               },
             }"
+            @change="onChange"
           >
             <template #item="{ item }" class="border border-yellow-300">
               <div class="lg:flex">
-                <img :src="item.image" alt="" />
+                <img
+                  :src="item.image"
+                  :alt="item.label"
+                  class="hidden lg:block"
+                />
                 <article>
                   <h2>{{ item.label }}</h2>
                   <p>{{ item.content }}</p>
@@ -39,6 +57,7 @@
 <script setup>
 const { planets } = usePlanet()
 
+// Data for the tabs
 const items = planets.map((planet) => ({
   label: planet.name,
   index: planet.name,
@@ -46,8 +65,13 @@ const items = planets.map((planet) => ({
   travelTime: planet.travelTime,
   distance: planet.distance,
   image: planet.image,
-  // Add more properties here if needed, like distance and travelTime
 }))
+
+// Changes the planet image shown up to lg according to the tab that is selected
+const selectedTabIndex = ref(0)
+function onChange(index) {
+  selectedTabIndex.value = index
+}
 </script>
 <style scoped>
 .bg-image {
