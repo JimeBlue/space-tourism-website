f82a55fc9a706d344ab66c9395a60e6fe596e06c
Jimena <j.miguez@airlst.com>
2024-01-30 15:53:53 +0100

    Add bg-image to crew page

--- pages/crew.vue
+++ pages/crew.vue
@@ -1,3 +1,26 @@
 <template>
-  <p>crew page</p>
+  <div class="bg-image min-h-screen w-screen">
+    <p>crew page</p>
+  </div>
 </template>
+<style scoped>
+.bg-image {
+  background: url('images/background-crew-mobile.jpg') no-repeat center center
+    fixed;
+  background-size: cover;
+}
+
+@media only screen and (min-width: 700px) {
+  .bg-image {
+    background-image: url('images/background-crew-tablet.jpg');
+    background-size: cover;
+  }
+}
+
+@media only screen and (min-width: 1024px) {
+  .bg-image {
+    background-image: url('images/background-crew-desktop.jpg');
+    background-size: cover;
+  }
+}
+</style>
